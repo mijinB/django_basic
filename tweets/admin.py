7eb4e19343a87585b29cbc4e654be75be0dc9d74
mijinB <bmj13465@gmail.com>
2024-07-02 21:55:54 +0900

    search, filter custom

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("elon musk", "Elon Musk"),
+            ("! elon musk", "! Elon Musk"),
+        ]
+
+    def queryset(self, request, tweets):
+        word = self.value()
+        if word:
+            if word.startswith("!"):
+                return tweets.exclude(payload__icontains=word[2:])
+            else:
+                return tweets.filter(payload__contains=word)
+        else:
+            tweets
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = (
@@ -10,7 +31,14 @@ class TweetAdmin(admin.ModelAdmin):
         "number_of_likes",
         "created_at",
     )
-    list_filter = ("created_at",)
+    list_filter = (
+        WordFilter,
+        "created_at",
+    )
+    search_fields = (
+        "payload",
+        "user__name",
+    )
 
 
 @admin.register(Like)
@@ -22,3 +50,4 @@ class LikeAdmin(admin.ModelAdmin):
         "created_at",
     )
     list_filter = ("created_at",)
+    search_fields = ("user__name",)
