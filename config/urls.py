211caacbe7ea5bac30436943219797b9195d6fa4
Bmijin <bmj13465@jtts.co.kr>
2024-07-04 13:28:35 +0900

    django template

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for config project.
+"""django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tweets/", include("tweets.urls")),
 ]
