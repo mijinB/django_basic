9081834bf762e2fdac33f8df1c9af726b795fd62
mijinB <bmj13465@gmail.com>
2024-07-08 22:22:08 +0900

    POST /api/v1/tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,21 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import TweetSerializer
 from .models import Tweet
 
 
 class Tweets(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         tweets = Tweet.objects.all()
         serializer = TweetSerializer(tweets, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TweetSerializer(data=request.data)
+        if serializer.is_valid():
+            tweet = serializer.save(user=request.user)
+            serializer = TweetSerializer(tweet)
+            return Response(serializer.data)
