caed4c47de45d1902cb19d49b6cb9b486075c54c
mijinB <bmj13465@gmail.com>
2024-07-08 22:19:29 +0900

    GET /api/v1/tweets

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Tweet
+from users.serializers import UserSerializer
 
 
 class TweetSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Tweet
-        fields = "__all__"
+        fields = (
+            "pk",
+            "user",
+            "payload",
+            "created_at",
+        )
