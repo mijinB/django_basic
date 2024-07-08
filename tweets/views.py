6c78e51bbb7cf487d3d7eeef9512a76e37fcdcc0
mijinB <bmj13465@gmail.com>
2024-07-08 22:23:37 +0900

    GET /api/v1/tweets/<int:pk>

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import NotFound
 from .serializers import TweetSerializer
 from .models import Tweet
 
@@ -19,3 +20,18 @@ class Tweets(APIView):
             tweet = serializer.save(user=request.user)
             serializer = TweetSerializer(tweet)
             return Response(serializer.data)
+
+
+class TweetDetail(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Tweet.objects.get(pk=pk)
+        except Tweet.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        tweet = self.get_object(pk)
+        serializer = TweetSerializer(tweet)
+        return Response(serializer.data)
