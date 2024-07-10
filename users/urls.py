836099e050ca33384fcc815a6d997a7ebc374897
Bmijin <bmj13465@jtts.co.kr>
2024-07-10 11:30:35 +0900

    PUT /api/v1/users/password (change password)

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Users.as_view()),
+    path("password", views.ChangePassword.as_view()),
     path("<int:pk>", views.UserDetail.as_view()),
     path("<int:pk>/tweets", views.UserTweets.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound, ParseError
+from rest_framework import status
 from .models import User
 from .serializers import UserSerializer, UserDetailSeializer
 from tweets.models import Tweet
@@ -27,6 +28,23 @@ class Users(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            raise ParseError
 
 
 class UserDetail(APIView):
