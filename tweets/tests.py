366a2d87d04650a984388b8548834fcee38b3c59
Bmijin <bmj13465@jtts.co.kr>
2024-07-18 08:52:21 +0900

    test 코드 작성

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,125 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from . import models
+from users.models import User
 
-# Create your tests here.
+
+class TestTweets(APITestCase):
+
+    PAYLOAD = "Test Tweet"
+    URL = "/api/v1/tweets/"
+
+    def setUp(self):
+        user = User.objects.create(username="test", )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+        models.Tweet.objects.create(
+            payload=self.PAYLOAD,
+            user=self.user,
+        )
+
+    def test_get_tweets(self):
+        response = self.client.get(self.URL)
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Status code isn't 200.",
+        )
+
+        self.assertIsInstance(
+            data,
+            list,
+        )
+        self.assertEqual(
+            len(data),
+            1,
+        )
+        self.assertEqual(
+            data[0]["payload"],
+            self.PAYLOAD,
+        )
+
+    def test_create_tweet(self):
+        new_tweet_payload = "New Tweet"
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "payload": new_tweet_payload,
+            },
+        )
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(self.user)
+        response = self.client.post(
+            self.URL,
+            data={
+                "payload": new_tweet_payload,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+
+        self.assertEqual(
+            data["payload"],
+            new_tweet_payload,
+        )
+
+        response = self.client.post(self.URL)
+        self.assertEqual(response.status_code, 400)
+
+
+class TestTweet(APITestCase):
+    PAYLOAD = "Test Tweet"
+    BASE_URL = "/api/v1/tweets/"
+
+    def setUp(self):
+        user = User.objects.create(username="test", )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+        models.Tweet.objects.create(
+            payload=self.PAYLOAD,
+            user=self.user,
+        )
+
+    def test_tweet_not_found(self):
+        response = self.client.get(f"{self.BASE_URL}2")
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_tweet(self):
+        response = self.client.get(f"{self.BASE_URL}1")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+
+        self.assertEqual(
+            data["payload"],
+            self.PAYLOAD,
+        )
+
+    def test_put_tweet(self):
+        self.client.force_login(self.user)
+        response = self.client.put(
+            f"{self.BASE_URL}1",
+            data={
+                "payload": self.PAYLOAD,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.put(
+            f"{self.BASE_URL}1",
+            data={
+                "payload": "",
+            },
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_delete_tweet(self):
+        self.client.force_login(self.user)
+        response = self.client.delete(f"{self.BASE_URL}1")
+        self.assertEqual(response.status_code, 200)
--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound, NotAuthenticated
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.exceptions import NotFound
-from rest_framework.status import HTTP_204_NO_CONTENT
 from .serializers import TweetSerializer
 from .models import Tweet
 
 
 class Tweets(APIView):
+
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request):
@@ -21,9 +22,15 @@ class Tweets(APIView):
             tweet = serializer.save(user=request.user)
             serializer = TweetSerializer(tweet)
             return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class TweetDetail(APIView):
+
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_object(self, pk):
@@ -39,18 +46,22 @@ class TweetDetail(APIView):
 
     def put(self, request, pk):
         tweet = self.get_object(pk)
-        serializer = TweetSerializer(
-            tweet,
-            request.data,
-        )
+        if tweet.user != request.user:
+            raise NotAuthenticated
+        serializer = TweetSerializer(tweet, data=request.data)
         if serializer.is_valid():
-            updated_tweet = serializer.save()
-            serializer = TweetSerializer(updated_tweet)
+            tweet = serializer.save()
+            serializer = TweetSerializer(tweet)
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
     def delete(self, request, pk):
         tweet = self.get_object(pk)
+        if tweet.user != request.user:
+            raise NotAuthenticated
         tweet.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return Response(status=HTTP_200_OK, )
